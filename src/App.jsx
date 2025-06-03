0f8aab42d8bc98c82163dec92633490c2daaeb99
fabioadelfio <adelfiofabio07@gmail.com>
2025-06-03 16:22:30 +0200

    posts table

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import DefaultLayout from './components/layouts/DefaultLayout';
 import Homepage from './pages/Homepage';
--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,11 +1,42 @@
+import axios from "axios";
+import { useEffect, useState } from "react"
+import { Link } from "react-router-dom";
+
 export default function Posts () {
+    const apiUrl = `https://lanciweb.github.io/demo/api/actresses/`;
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get(apiUrl)
+            .then(res => setPosts(res.data))
+            .catch(err => console.error(`Errore nel caricamento dei dati!`));
+    }, []);
+
     return (
         <div className="container">
-            <ul className="posts">
-                <li>Post 1</li>
-                <li>Post 2</li>
-                <li>Post 3</li>
-            </ul>
+            <h1>Lista dei Post</h1>
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>NAME</th>
+                        <th>LINKS</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {posts.map(post => (
+                        <tr>
+                            <td>{post.id}</td>
+                            <td>{post.name}</td>
+                            <td>
+                                <Link to={`/posts/${post.id}`} className="btn btn-primary">Show</Link>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     )
 }
